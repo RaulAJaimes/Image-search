5857ac41c31e8ebed3c2c558d7482aea0c97011f
RaulAJaimes <albertojara@ufps.edu.co>
2023-03-11 22:14:39 -0500

    component result created

--- src/componentes/Buscador.js
+++ src/componentes/Buscador.js
@@ -4,14 +4,16 @@ class Buscador extends Component {
     busquedaRef = React.createRef();
 
     obtenerDatos = (e) => {
+        e.preventDefault();
 
+        console.log(this.busquedaRef.current.value);
     }
 
     render(){
         return(
             <form onSubmit={this.obtenerDatos}>
-                <div className="row">
-                    <h5 style={{color: '#53C1BB'}}>Buscador de Imagenes</h5>
+                <div className="container row">                    
+                    <h5 style={{color: '#53C1BB'}}>Buscador de Imagenes</h5>                    
                     <div className="form-group col-md-8">                        
                         <input ref={this.busquedaRef} type={'text'} className='form-control form-control-lg' placeholder="Buscar imagen"/>
                     </div>
